c03d4556d60e2d7e7f2ce5021f4dbd604c5725c9
@SoulOG <285963@supinfo.com>
2021-06-20 22:35:43 +0000

    Fin de l'implementation de la fonction Completion

---
+++ Fleissner.py
@@ -0,0 +1,71 @@
+import random
+import string
+
+
+def convertLetters(text):
+    for i in range(len(text) - 1):
+        # Removing the space
+        if text[i] == ' ':
+            text = text.replace(text[i], '')
+            return text
+    return text
+
+
+def completion(text, n):
+    aleaStr = ''
+    alpha = string.ascii_lowercase
+    for i in range(n):
+        alea = random.randint(0, 25)
+        aleaStr += alpha[alea]
+    text += aleaStr
+    return text
+
+
+def rotation(grid):
+    tab = []
+    for i in range(len(grid)):
+        for k in range(len(grid[i])):
+            tab.append(grid[k][len(grid) - 1 - i])
+    return tab
+
+
+def correct(grid, n):
+    etat = False
+    return etat
+
+
+def randomGrid(n):
+    grid = []
+    return grid
+
+
+def loadGrid(file):
+    pass
+
+
+def saveGrid(grid, n, file):
+    pass
+
+
+def cipher(grid, n, text, clock):
+    pass
+
+
+def squareToText(square, n):
+    pass
+
+
+def cipherFleissner(grid, n, text, clock):
+    pass
+
+
+def textToSquare(text, n):
+    pass
+
+
+def decipher(grid, n, square, clock):
+    pass
+
+
+def decipherFleissner(grid, n, text, clock):
+    pass
---
+++ test.py
@@ -0,0 +1,18 @@
+import re
+import random
+import string
+
+txt = "gangsta rap! iu éééééeee"
+x = re.sub('é', "e", txt)
+
+y = 'Garo'
+aleaStr = ''
+alpha = string.ascii_lowercase
+print(alpha)
+
+for i in range(5):
+    alea = random.randint(0, 25)
+    aleaStr += alpha[alea]
+
+print(aleaStr)
+print(y)
